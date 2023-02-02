ef4620a146244ea938808e56e9cb103f0e5561c6
Stickman008 <arm.supakorn@gmail.com>
2023-02-02 10:56:37 +0700

    Update server package

--- server/server.js
+++ server/server.js
@@ -1,2 +1,36 @@
-const Express = require('express')
+require("dotenv").config();
 
+const express = require("express");
+const mongoose = require("mongoose");
+
+// express app
+const app = express();
+
+// middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+  });
+  
+// routes
+app.get("/", (req, res) => {
+    res.json({ mess: "main!" });
+  });
+// app.use("/api/user", userRoutes);
+
+// connect to database
+const PORT = process.env.PORT || 4000;
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(PORT, () => {
+      console.log("connected to db & listening on port", PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
