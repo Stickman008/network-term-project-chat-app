9dc9e2e528bad0e230d4d08d6214478807a562f1
podesport <88339411+podesport@users.noreply.github.com>
2023-04-21 16:40:25 +0700

    start chat page (by nick)

--- client/app/chat/[chatId]/page.js
+++ client/app/chat/[chatId]/page.js
@@ -1,8 +1,17 @@
+import Navbar from "../../../component/navbar";
+import Sidebar from "../../../component/sidebar";
 export default function page(params) {
   return (
     <>
-      <p>chat</p>
-      <p>test</p>
+      <Navbar />
+      <div className="row">
+        <div className="col-4 col-md-2 bg-primary">
+          <Sidebar />
+        </div>
+        <div className="col-8 col-md-10 p-0">
+          <p className="bg-dark text-white"> hello world </p>
+        </div>
+      </div>
     </>
   );
 }
--- client/app/page.js
+++ client/app/page.js
@@ -11,6 +11,8 @@ export default function Home() {
     <>
       <Navbar />
       <p>home page</p>
+      <a class="btn btn-success" href="/chat" role="button">real but cant not use</a>
+      <a class="btn btn-secondary" href="/chat/:chat_id" role="button">not real but it working</a>
     </>
   );
 }
